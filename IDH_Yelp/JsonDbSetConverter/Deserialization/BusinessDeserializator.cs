39dc84bed41eea5ecfd686ab8aa589536be95455
Adzkyy <neuab95@gmail.com>
2021-01-28 00:12:42 +0100

    Wyświetlanie progresu

--- IDH_Yelp/JsonDbSetConverter/Deserialization/BusinessDeserializator.cs
+++ IDH_Yelp/JsonDbSetConverter/Deserialization/BusinessDeserializator.cs
@@ -1,4 +1,5 @@
 ﻿using JsonDbSetConverter.Data;
+using JsonDbSetConverter.Helpers;
 using Newtonsoft.Json;
 using System;
 using System.IO;
@@ -9,7 +10,10 @@ namespace JsonDbSetConverter.Deserialization
     {
         public static void DeserializeFromPath(string table, YelpContext context)
         {
+            Console.WriteLine();
             Console.WriteLine("\nŁaduje do bazy Business");
+            (var top, var left) = CursorHelper.GetCurPos();
+
             using (var streamReader = new StreamReader($@"C:\Users\neuab\Documents\IDH_Yelp\IDH_Yelp\DbSetJson\yelp_academic_dataset_{table}.json"))
             using (var jsonReader = new JsonTextReader(streamReader))
             {
@@ -27,7 +31,7 @@ namespace JsonDbSetConverter.Deserialization
                     recordLoaded++;
                     counter++;
 
-                    Console.SetCursorPosition(1, 1);
+                    Console.SetCursorPosition(0, 0);
                     Console.WriteLine(recordLoaded);
 
                     if (counter == 5000)
@@ -36,6 +40,9 @@ namespace JsonDbSetConverter.Deserialization
                         counter = 0;
                     }
                 }
+                context.SaveChanges();
+
+                Console.SetCursorPosition(left, top);
                 Console.WriteLine("Załadowałem Business");
             }
         }
---
+++ IDH_Yelp/JsonDbSetConverter/Helpers/CursorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonDbSetConverter.Helpers
+{
+    public static class CursorHelper
+    {
+        public static (int, int) GetCurPos()
+        {
+            var top = Console.CursorTop;
+            var left = Console.CursorLeft;
+            return (top, left);
+        }
+    }
+}
